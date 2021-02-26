e2a5f9941e0450e1d71a923a23993423570963fe
kriscfoster <kfoster@salesforce.com>
2021-02-26 15:00:19 +0000

    progress

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,24 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  modulePathIgnorePatterns: ['<rootDir>/build/']
+  rootDir: "..",
+  clearMocks: true,
+  coverageDirectory: "coverage",
+  collectCoverageFrom: ["src/**/*.ts"],
+  coveragePathIgnorePatterns: [
+    "/node_modules/",
+    "types\\.ts",
+    "index\\.ts",
+    ".+\\.d\\.ts"
+  ],
+  globals: {
+    "ts-jest": {
+      tsConfig: "src/__tests__/tsconfig.json"
+    }
+  },
+  testPathIgnorePatterns: ["/node_modules/", "/fixtures/", "/utils/"],
+  moduleFileExtensions: ["ts", "tsx", "js"],
+  setupFilesAfterEnv: ["<rootDir>/test/jest.setup.ts"],
+  testEnvironment: "node",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest"
+  }
 };
